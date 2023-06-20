8eb303816f0e5a554c6b4873084c96d33f3e6e56
sam <916878440@qq.com>
2023-06-20 23:16:17 +0800

    修复依赖冲突问题

--- app/src/main/java/com/example/dial/MainActivity.kt
+++ app/src/main/java/com/example/dial/MainActivity.kt
@@ -4,26 +4,22 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.dial.ui.theme.DialTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            DialTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    DialView(start = 4, end = 16, editType = DutyTimeEditType.EDIT)
-                }
+            Surface(
+                modifier = Modifier.fillMaxSize(),
+                color = MaterialTheme.colors.background
+            ) {
+                DialView(start = 4, end = 16, editType = DutyTimeEditType.EDIT)
             }
         }
     }
@@ -40,7 +36,5 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    DialTheme {
-        Greeting("Android")
-    }
+    Greeting("Android")
 }
